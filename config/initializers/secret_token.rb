351f2f1edd86df81c35acb6902aa73f2435158c5
ErikBean <erikbean08@gmail.com>
2013-03-24 00:09:01 -0700

    inital setup with mysql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SandBox::Application.config.secret_token = '8064cf98281a28df02472072187e9f9cdcc02ed3d17026c46137bb0e374d697a332cc63cb632d443c9240d546c1d0e63b42aecf38efe4a5bf14c44da5b504695'
+SandBox::Application.config.secret_token = '54b5db276d66a76e84dac078e32ee9511013a8d7d5c443dcc0b6ca1e9f10906cd6980bbd16400c499e959883a3e23e29b4af6caa1baa2f053e915d845552260f'
