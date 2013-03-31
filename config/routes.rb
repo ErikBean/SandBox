c8ef473073f62a66dade73d2072bfd0557a3aba6
ErikBean <erikbean08@gmail.com>
2013-03-31 12:56:41 -0700

    highlighted tabs, sliding doors, some css boxes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SandBox::Application.routes.draw do
   get "home/index"
+  root :to => 'home#index'
+  match "/news" => "home#news"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +52,7 @@ SandBox::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
